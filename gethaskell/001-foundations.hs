ce94fc0e4e195c63d656d60d3e87ee8fdd534be7
nachoba <0800nacho@gmail.com>
2018-05-13 18:24:04 -0300

    new book

---
+++ gethaskell/001-foundations.hs
@@ -0,0 +1,29 @@
+{-
+    01  FOUNDATIONS OF FUNCTIONAL PROGRAMMING
+    -----------------------------------------
+    The foundations of functional programming are abstract, mathematical notions
+    of computation that trascend a specific implementation. This leads to  a me-
+    thod of programming that often solves problems simply by describing them. By
+    focusing on computation,not computers,functional programming allows the pro-
+    grammer access to powerful abstractions  that can make many challenging pro-
+    blems much easier to solve.
+
+    Haskell is the purest functional programmimg language you can learn, it does
+    not allow you to stray back to more familiar styles of programming. 
+
+
+    Lesson 02 :: Functions and Functional Programming
+    -------------------------------------------------
+    In this section you will be able to:
+        * Understand the general idea of functional programming
+        * Define simple functions in Haskell
+        * Declare variables in Haskell
+        * Explain the benefits of functional programming
+
+    
+
+
+
+-}
+
+
